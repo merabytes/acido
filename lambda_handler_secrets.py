97edd9430b07308d11e4c0cf15612f3a1280cffd
kn0wm4d <xalvarez@merabytes.com>
2025-11-06 13:02:30 +0100

    fix: publish 0.40.1

--- lambda_handler_secrets.py
+++ lambda_handler_secrets.py
@@ -39,7 +39,7 @@ def _handle_healthcheck():
     return build_response(200, {
         'status': 'healthy',
         'message': 'Lambda function is running',
-        'version': '0.47.0'
+        'version': '0.40.1'
     }, CORS_HEADERS)
 
 
